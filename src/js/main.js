01a9f293cedd4e2df5bdb6cea77604d461ec9b3b
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-03-09 00:41:16 +0900

    ポートフォリオ、ポートフォリオのモーダル、Career、Skills のレイアウトが完成

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 // ###################################################################
 //    JS エントリーポイント
 // ###################################################################
-const $ = require('jquery');
-require('bootstrap');
+const $ = require('../../node_modules/jquery/dist/jquery');
+const Swiper = require('../../node_modules/swiper/js/swiper');
+const Chart = require('../../node_modules/chart.js/dist/Chart');
+require('../../node_modules/bootstrap/dist/js/bootstrap');
 
 $(() => {
   // スムーズスクロール
@@ -35,4 +37,65 @@ $(() => {
   };
   navbarCollapse();
   $(window).scroll(navbarCollapse);
+
+  // ポートフォリオモーダル内のSwiper
+  $('.js-portfolio-modal').on('shown.bs.modal', e => {
+    const $this = $('.swiper-container', e.currentTarget);
+    const index = $('.swiper-container').index($this);
+    if ($this.data('swiper')) {
+      // 既に初期化済み
+      $this.data('swiper').updateAutoHeight(100);
+      return;
+    }
+
+    $this.data('swiper', new Swiper(
+      `.swiper-container-portfolio-${index + 1}`, Object.assign({
+        slidesPerView: 1,
+        autoHeight: true
+      }, ($('.swiper-slide', $this).length <= 1) ? {} : {
+        loop: true,
+        pagination: {
+          el: `.swiper-pagination-portfolio-${index + 1}`,
+          type: 'bullets',
+          clickable: true
+        },
+        navigation: {
+          prevEl: `.swiper-button-prev-portfolio-${index + 1}`,
+          nextEl: `.swiper-button-next-portfolio-${index + 1}`
+        }
+      })
+    ));
+    $this.data('swiper').updateAutoHeight(100);
+  });
+
+  // スキルセクションのレーダーチャート
+  $('.js-skills-chart').each((index, el) => {
+    const $this = $(el);
+    const $canvas = $('.js-skills-chart-canvas', el);
+    const chart = JSON.parse($('.js-skills-chart-data', $this).val().replace(/'/g, '"'));
+
+    $this.data('chart', new Chart($canvas[0], {
+      type: chart.type,
+      data: chart.data,
+      options: {
+        title: {
+          display: true,
+          fontSize: 24,
+          padding: 5,
+          text: chart.title
+        },
+        legend: {
+          display: false
+        },
+        scale: {
+          ticks: {
+            min: 0,
+            max: 100,
+            fontColor: 'rgba(127, 127, 127, 0.5)',
+            backgroundColor: 'rgba(0, 0, 0, 0)'
+          }
+        }
+      }
+    }));
+  });
 });
