5745e2e2e3dc28c420cca0ee5e3160db85554e3f
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-03-09 02:58:26 +0900

    Contact セクション完成、container-fluid 廃止

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 const $ = require('../../node_modules/jquery/dist/jquery');
 const Swiper = require('../../node_modules/swiper/js/swiper');
 const Chart = require('../../node_modules/chart.js/dist/Chart');
+const Base64 = require('../../node_modules/js-base64/base64').Base64;
 require('../../node_modules/bootstrap/dist/js/bootstrap');
 
 $(() => {
@@ -98,5 +99,22 @@ $(() => {
         }
       }
     }));
+  });
+
+  // コンタクトセクションのデコード
+  $('.js-contact-item').each((index, el) => {
+    const $this = $(el);
+    if ($('input.js-encode', $this).length === 0) {
+      // 非エンコードの項目
+      return;
+    }
+
+    // エンコードされた項目をデコード
+    let href = $('.js-address-link', $this).attr('href');
+    let text = $('.js-address-link-text', $this).text().trim();
+    href = Base64.decode(href);
+    text = Base64.decode(text);
+    $('.js-address-link', $this).attr('href', href);
+    $('.js-address-link-text', $this).text(text);
   });
 });
